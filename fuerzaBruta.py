522f86624e9ed5cf75cd37b0a6406d4626f77638
advargas488 <70667536+advargas488@users.noreply.github.com>
2021-09-05 16:43:48 -0600

    Update fuerzaBruta.py

--- fuerzaBruta.py
+++ fuerzaBruta.py
@@ -2,7 +2,8 @@ import dominoes
 from copy import deepcopy
 import sys
 
-board = dominoes.create_puzzle(2)
+soluciones = []
+
 
 def check_tiles(tiles):
     i = 0
@@ -15,31 +16,12 @@ def check_tiles(tiles):
             else:
                 j += 1
         i += 1
-        k+=1
+        k += 1
     return True
 
-"""
-2 2 1 3 0 
-0 2 0 2 1 
-0 0 1 3 2 
-3 3 1 3 1 
 
-"""
-
-def fuerza_bruta(board):
+def generar_solucion(A, board):
     i = 0
-    j = 0
-    solution = []
-    tiles = []
-    ori = 0
-    n_tiles = int(len(board) * (len(board[0])/2))
-    sol = [None] * n_tiles
-    solucion = generateAllBinaryStrings(n_tiles,board,0,sol)
-    return solucion
-
-
-def arreglo_binario(A, n, board): 
-    i = 0 
     j = 0
     k = 0
     mirror = deepcopy(board)
@@ -51,62 +33,75 @@ def arreglo_binario(A, n, board):
             tiles += [[board[j][k]]+[board[j][k+1]]]
             mirror[j][k] = "V"
             mirror[j][k+1] = "V"
-            k+=2
+            k += 2
 
-            if i+1 < len(A) and  (A[i+1] == 0 and len(board[0])-k == 1 or len(board[0])-k == 0):
-                k=0
-                j+=1
+            if i+1 < len(A) and (A[i+1] == 0 and len(board[0])-k == 1 or len(board[0])-k == 0):
+                k = 0
+                j += 1
             elif i+1 < len(A) and (A[i+1] == 1 and len(board)-j == 1 or k > len(board[0])):
-                j+=1
-                k=0
+                j += 1
+                k = 0
         elif A[i] == 1 and mirror[j][k] != "V" and len(board) > j+1 and mirror[j+1][k] != "V":
             tiles += [[board[j][k]]+[board[j+1][k]]]
             mirror[j][k] = "V"
-            mirror[j+1][k] ="V"
-            k+=1
+            mirror[j+1][k] = "V"
+            k += 1
 
             if i+1 < len(A) and (A[i+1] == 0 and (len(board[0])-k == 1 or len(board[0])-k == 0)):
                 k = 0
                 j += 1
             elif i+1 < len(A) and (A[i+1] == 1 and (len(board)-j == 1 or len(board[0]) <= k)):
-                j+=1
-                k=0
+                j += 1
+                k = 0
         else:
-            k+=1
+            k += 1
             if k >= len(board[0]) or (A[i] == 0 and (len(board[0])-k == 1 or len(board[0])-k == 0)):
                 k = 0
                 j += 1
-                if j>=len(board):
+                if j >= len(board):
                     i = len(A)
                     complete = False
                     break
-                else: 
+                else:
                     continue
             else:
                 continue
 
         if (A[i] == 0 and k+1 > len(board[0])) or (A[i] == 1 and j+1 > len(board)) or j >= len(board):
-                i = len(A)
-                complete = False
-                break
+            i = len(A)
+            complete = False
+            break
         else:
             complete = True
 
-        i+=1      
-    if complete == True:
-        if check_tiles(tiles) == True:
-            return A
+        i += 1
+    if complete == True and check_tiles(tiles) == True:
+        return True
+    else:
+        return False
 
-def generateAllBinaryStrings(n, board, i,A):
-    
+
+def generateAllBinaryStrings(n, board, i, A):
+    global soluciones
     if i == n:
-        if arreglo_binario(A,n,board) != None:
-            return arreglo_binario(A,n,board)
+        soluciones += deepcopy([A])
         return
+
     A[i] = 0
-    generateAllBinaryStrings(n,board, i + 1, A)
+    generateAllBinaryStrings(n, board, i + 1, A)
 
     A[i] = 1
     generateAllBinaryStrings(n, board, i + 1, A)
-print(board)
-print(fuerza_bruta(board))
+
+
+def fuerza_bruta(board):
+    done = False
+    n_tiles = int(len(board) * (len(board[0])/2))
+    sol = [None] * n_tiles
+    generateAllBinaryStrings(n_tiles, board, 0, sol)
+    for item in soluciones:
+        if done:
+            break
+        elif generar_solucion(item, board):
+            done = True
+            return item
